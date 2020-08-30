a4287e5322035a266c247ce036126b6d6ad648a7
Carmen Macleroy Obied, PhD <59775836+carmenobied@users.noreply.github.com>
2020-08-30 16:28:58 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
-// import Search from "./pages/Search";
+import Search from "./pages/search";
 import Nav from "./components/Nav";
 
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <Nav />
+const App = () => (
+  <Router>
+    <div>
+      <Nav />
         <Switch>
           <Route exact path={["/", "/books"]}>
             <Books />
           </Route>
           <Route exact path="/books/:id">
-            {/* <Detail /> */}
+            <Detail />
           </Route>
-          {/* <Route exact path="/bookshelf" component={Bookshelf} /> */}
-          {/* <Route exact path="/search" component={Search} /> */}
+           <Route exact path="/search">
+            <Search />
+          </Route>
         </Switch>
-      </div>
-    </Router>
-  );
-}
+    </div>
+  </Router>
+);
 
 export default App;
-
-
