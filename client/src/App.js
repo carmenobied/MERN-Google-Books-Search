6fe4f7895256c135729974a0dd831017db48f867
Carmen Macleroy Obied, PhD <carmen.obied@gmail.com>
2020-08-29 23:05:36 -0700

    created api routes, controllers, models, seedDB on the server side. created components, pages and api util on the client side. installed dependencies, added bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,58 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
-function App() {
+// The app will not render correctly until you setup a Route component.
+// Refer to the Basic Example documentation if you need to.
+// (https://reacttraining.com/react-router/web/example/basic)
+
+
+// You can use the last <Route> in a <Switch> as a kind of
+// "fallback" route, to catch 404 errors.
+//
+// There are a few useful things to note about this example:
+//
+// - A <Switch> renders the first child <Route> that matches
+// - A <Redirect> may be used to redirect old URLs to new ones
+// - A <Route path="*> always matches
+const App = () => {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <Router>
+        <Nav />
+        <Switch>
+          {/* The `/` and `/books` routes should both render the `Books` component page. */}
+          <Route exact path={["/", "/books"]}>
+            <Books />
+          </Route>
+          <Route exact path="/books/:id">
+            <Detail />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+              {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+    </Router>
   );
 }
 
-
 export default App;
+
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
