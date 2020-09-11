9756d1e99cca7097b829fdf43c68c6c9976e9b0f
Carmen Macleroy Obied, PhD <carmen.obied@gmail.com>
2020-09-10 17:24:01 -0700

    further updates to api routes

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,20 +1,14 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
 
-// Search for books via Google Books API
-router.route("/api/books/search/")
-  .get(BASEURL + query.title);
-
 // Matches with "/api/books"
 router.route("/")
-  .get(booksController.findAll)
-  .post(booksController.create);
+    .get(booksController.findAll)
+    .post(booksController.create);
 
 // Matches with "/api/books/:id"
-router
-  .route("/:id")
-  .get(booksController.findById)
-  .put(booksController.update)
-  .delete(booksController.remove);
+router.route("/:id")
+    .get(booksController.findById)
+    .delete(booksController.remove);
 
 module.exports = router;
